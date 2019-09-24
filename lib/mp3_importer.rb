4bcb42cd661082ec94de7ea7db500c763cad3dcc
Matt Plichta <bigcatplichta@gmail.com>
2019-09-24 14:52:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,9 @@ class MP3Importer
   end 
   
   def files
-    @files = Dir.children(@path) #.each {|f| f.gsub!(".mp3", "")}
+    @files = Dir.children(@path) 
+    
+    #.each {|f| f.gsub!(".mp3", "")}
     
     #binding.pry
   end 
