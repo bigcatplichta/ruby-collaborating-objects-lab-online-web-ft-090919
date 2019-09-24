ce6cf6d3994516eecf8791fa205fec6d4d419692
Matt Plichta <bigcatplichta@gmail.com>
2019-09-24 06:28:46 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class MP3Importer
+  attr_reader :path 
+  
+  def initialize(path)
+    @path = path
+    
+  end 
+  
+  def files
+    @files = Dir.glob("#{@path}/*.mp3").each do |f| 
+     f.delete(@path)
+      binding.pry
+    end
+    
+     
+    
+    
+    
+    
+  end 
+  
+  def import 
+    #use Song.new_by_filename to create from list of files
+  end 
+end 
